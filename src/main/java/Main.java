6f6c95b4994dce35979b4bc88a8331fb0f58d9c6
Josue <josue@zipcodersmbp75.lan>
2024-08-14 20:30:25 -0400

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         Person person = new Person();
-        Person eyan = new Person(5,"Jeffrey", "Eyan", "New Place", "New City!");
-        person.update(eyan);
+        person.findAll();
     }
 }
